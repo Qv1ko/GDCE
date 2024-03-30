97b4f0ffe3ab75e809fb37b4e5a2202852cfea53
Víctor García <vicgarmur947@gmail.com>
2024-03-30 22:53:11 +0000

    Lector de códigos QR y buscador de portátiles añadidos a la página de Inicio

--- views/site/index.php
+++ views/site/index.php
@@ -7,48 +7,74 @@
 
     $this->title = 'Inicio';
 
+    $this->registerJsFile('@web/js/lector_qr.js', ['position' => \yii\web\View::POS_HEAD]);
+
 ?>
 
 <div class="site-index">
 
-    <script src="https://cdn.rawgit.com/mebjas/html5-qrcode/minified/html5-qrcode.min.js"></script>
-
-    <div id="reader" style="width:300px;height:300px"></div>
-
-    <script>
-        function onScanSuccess(decodedText, decodedResult) {
-            // maneja el resultado del escaneo
-            console.log(`QR code with decodedText=${decodedText}`, decodedResult);
-        }
-
-        var html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader", { fps: 10, qrbox: { width: 250, height: 250 } });
-        html5QrcodeScanner.render(onScanSuccess);
-    </script>
-
-    <div class="row">
-        <div class="col-md-6">
+    <div class="row justify-content-center">
+        <div id="reader" class="col-6"></div>
+        <div id="result"></div>
+        <div class="text-center col-12">
+            <h3>Escanea el código QR <br> o <br> Ingresa el código del portátil</h3>
+        </div>
+        <div class="col-md-2 col-sd-3">
             <div class="input-group">
-                <input type="text" id="searchInput" class="form-control" placeholder="Ingresa el código del portátil (ej. 123A)">
+                <input type="text" id="searchInput" class="form-control" placeholder="(ej. 123A)">
                 <div class="input-group-append">
-                    <button class="btn btn-primary" onclick="search()">Buscar</button>
+                    <button class="btn btn-primary" onclick="buscar(document.getElementById('searchInput').value)">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-search">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"/>
+                            <path d="M21 21l-6 -6"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-        function search() {
-            var inputText = document.getElementById('searchInput').value;
-            // Expresión regular para validar el patrón
-            var regex = /^\d{3}[a-zA-Z]$/;
-            if (regex.test(inputText)) {
-                window.location.href = 'site/portatil?portatil=' + inputText;
-            } else {
-                alert("El código del portátil debe tener el formato correcto (ej. 123A)");
-            }
-        }
-    </script>
 
+        const scanner = new Html5QrcodeScanner('reader', { 
+            qrbox: {
+                width: 240,
+                height: 240,
+            },
+            fps: 20,
+        });
+
+
+        scanner.render(success, error);
+
+        function success(result) {
+
+            buscar(result);
+            
+            scanner.clear();
+            
+            document.getElementById('reader').remove();
+
+        }
+
+        function error(err) {
+            console.error(err);
+        }
+
+        function buscar(codigo) {
+
+            var patron = /^\d{3}[a-zA-Z]$/;
+
+            if (patron.test(codigo)) {
+                window.location.href = 'site/portatil?portatil=' + codigo;
+            } else {
+                alert('El código del portátil debe tener el formato correcto (ej. 123A)');
+                location.reload();
+            }
+            
+        }
+        
+    </script>
 
 </div>
