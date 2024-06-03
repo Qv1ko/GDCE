ff20b0b8ffdb2fd414e19e82ed61d6ba16c1b857
Víctor García <vicgarmur947@gmail.com>
2024-06-03 19:00:39 +0100

    Corrección del botón del modal de actualización de cursos

--- views/cursos/_updateForm.php
+++ views/cursos/_updateForm.php
@@ -57,13 +57,12 @@
 
             <div class="row d-flex justify-content-around">
                 <?= Html::submitButton('<div class="d-flex align-items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-square-plus" style="margin-right: 4px;">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-refresh" style="margin-right: 4px;">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                        <path d="M9 12h6" />
-                        <path d="M12 9v6" />
-                        <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+                        <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+                        <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
                     </svg>
-                    <span>Guardar</span>
+                    <span>Actualizar</span>
                 </div>', ['class' => 'btn btn-success']) ?>
                 <?= Html::button('<div class="d-flex align-items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-square-x" style="margin-right: 4px;">
