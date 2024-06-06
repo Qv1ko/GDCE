52134c6296b42acc1e28799341fe187b290f8087
Víctor García <vicgarmur947@gmail.com>
2024-06-06 01:14:00 +0100

    Ajustados los elementos del footer para que reaccionen correctamente con las dimensiones de la pantalla

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -82,17 +82,19 @@
         </div>
     </main>
 
-    <footer class="footer mt-auto py-3 text-muted d-flex align-items-center justify-content-between">
+    <footer class="footer mt-auto py-3 text-muted" style="text-align: center;">
         <div class="container">
-            <p class="float-left" style="color: #E3E3E3;">Desarrollado por <span style="color: #82C0CC">Víctor García Murillo</span></p>
-            <div>
-                <a class="float-right footer-icon" href="https://github.com/Qv1ko/GDCE" target="_blank">
+            <div class="flex-grow-1">
+                <p style="color: #E3E3E3;">Desarrollado por <span style="color: #82C0CC">Víctor García Murillo</span></p>
+            </div>
+            <div class="flex-shrink-0">
+                <a class="footer-icon" href="https://github.com/Qv1ko/GDCE" target="_blank">
                     <svg class="button-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512">
                         <title>Proyecto en GitHub</title>
                         <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
                     </svg>
                 </a>
-                <a class="float-right footer-icon" href="https://gitlab.com/vgarcia3301646/gdce" target="_blank">
+                <a class="footer-icon" href="https://gitlab.com/vgarcia3301646/gdce" target="_blank">
                     <svg class="button-icon" xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 512 512">
                         <title>Proyecto en GitLab</title>
                         <path d="M503.5 204.6L502.8 202.8L433.1 21C431.7 17.5 429.2 14.4 425.9 12.4C423.5 10.8 420.8 9.9 417.9 9.6C415 9.3 412.2 9.7 409.5 10.7C406.8 11.7 404.4 13.3 402.4 15.5C400.5 17.6 399.1 20.1 398.3 22.9L351.3 166.9H160.8L113.7 22.9C112.9 20.1 111.5 17.6 109.6 15.5C107.6 13.4 105.2 11.7 102.5 10.7C99.9 9.7 97 9.3 94.1 9.6C91.3 9.9 88.5 10.8 86.1 12.4C82.8 14.4 80.3 17.5 78.9 21L9.3 202.8L8.5 204.6C-1.5 230.8-2.7 259.6 5 286.6C12.8 313.5 29.1 337.3 51.5 354.2L51.7 354.4L52.3 354.8L158.3 434.3L210.9 474L242.9 498.2C246.6 500.1 251.2 502.5 255.9 502.5C260.6 502.5 265.2 500.1 268.9 498.2L300.9 474L353.5 434.3L460.2 354.4L460.5 354.1C482.9 337.2 499.2 313.5 506.1 286.6C514.7 259.6 513.5 230.8 503.5 204.6z"/>
@@ -101,6 +103,31 @@
             </div>
         </div>
     </footer>
+
+    <style>
+        .footer .container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-wrap: wrap;
+        }
+
+        .footer .container > div {
+            flex-basis: 48%; /* Ajusta el valor según sea necesario para dejar espacio para el margen entre los elementos */
+        }
+
+        @media (max-width: 768px) {
+            .footer .container {
+                justify-content: center;
+                text-align: center;
+            }
+
+            .footer .container > div {
+                flex-basis: auto;
+                max-width: 100%;
+            }
+        }
+    </style>
 
     <?php $this->endBody() ?>
     </body>
